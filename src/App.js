2bd7c175187cf1d6420857d6c7c5f9e39684e6e8
raisuman0077 <raisuman0077@gmail.com>
2022-08-22 17:58:30 +0545

    constent/variable created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg: '', type:''});
+
   return <h2>grocery bud setup</h2>
 }
 
